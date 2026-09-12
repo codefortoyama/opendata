3eaa116f3bc1eda2494b968c82a25c7474e2f5e1
tominarievo <tominari@evolinq.link>
2026-09-13 01:23:15 +0900

    Configure site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://codefortoyama.github.io',
+  base: '/opendata',
   outDir: 'dist',
   trailingSlash: 'always',
   // GitHub Pages設定
